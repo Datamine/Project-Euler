12943021e05aa50178ddc649cb4b73f01930750f
Datamine <contact@johnloeber.com>
2017-02-01 01:23:16 -0600

    problem 28 solved

---
+++ Problems/027/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os, sys
+sys.path.append(os.getcwd())
+
+from Utilities import primes
+
+def main():
+    # caching the first thousand primes. it seems unlikely that we'll need more.
+    first_thousand_primes = set(primes.prime_numbers(1000))
+    max_number_of_primes = 0
+    corresponding_coefficients_product = 0
+
+    for a in range(-1000,1000):
+        for b in range(-1000,1000):
+            n = 0
+            while (n**2 + (a*n) + b) in first_thousand_primes:
+                n+=1
+            if n > max_number_of_primes:
+                max_number_of_primes = n
+                corresponding_coefficients_product = a * b
+
+    return corresponding_coefficients_product
+
+if __name__=='__main__':
+    print(main())
---
+++ Problems/028/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os, sys
+sys.path.append(os.getcwd())
+
+def main():
+    # think of the spiral as a set of nested squares, where the side length grows
+    # o-n every level by two (one integer on each side). then we try to capture all
+    # the corner points on that level.
+
+    # skipping the special case of the square of length one -- including that in the acc
+    accumulator = 1
+    # keep track of the number of values contained in previous squares/the maximum natural number up to now
+    number_of_values_covered = 1
+    for side_length in range(3,1002,2):
+        corner_one = number_of_values_covered + side_length - 1
+        corner_two = number_of_values_covered + 2 * (side_length - 1)
+        corner_three = number_of_values_covered + 3 * (side_length - 1)
+        corner_four = number_of_values_covered + 4 * (side_length - 1)
+        number_of_values_covered = corner_four
+        accumulator += corner_one + corner_two + corner_three + corner_four
+
+    return accumulator
+
+if __name__=='__main__':
+    print(main())
