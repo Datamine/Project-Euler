2939478a3cb4a0a437b70c2df509ea828cb5a259
Datamine <contact@johnloeber.com>
2017-01-31 23:51:16 -0600

    changed new-problem to use spaces instead of tabs

--- new-problem.py
+++ new-problem.py
@@ -8,7 +8,7 @@ if not os.path.exists(new_problem):
     os.mkdir(new_problem)
 
 filetext = ["#!/usr/bin/python3\n", "\n", "import os, sys\n", "sys.path.append(os.getcwd())\n",
-            "\n", "def main():\n", "\n", "if __name__=='__main__':\n", "\tprint(main())\n"]
+            "\n", "def main():\n", "\n", "if __name__=='__main__':\n", "    print(main())\n"]
 filepath = os.path.join(current_path, new_problem, "solution.py")
 
 with open(filepath, "w") as f:
